0ff7b8497aa3dd6557da68ac9c0f173613d1b16f
Ari <142460393+ariantonio@users.noreply.github.com>
2023-11-22 23:02:57 -0300

    Add files via upload

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+import view.TelaDeLogin;
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+    public static void main(String[] args){
+        TelaDeLogin login = new TelaDeLogin();
+        
+    }
+}
