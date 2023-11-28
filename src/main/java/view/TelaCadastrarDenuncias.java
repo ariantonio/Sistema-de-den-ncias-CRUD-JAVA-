7aba525909d613801d2263166a86ba7e93e2f582
Vitor <vitorsena78@gmail.com>
2023-11-27 23:17:13 -0300

    .

--- src/main/java/view/TelaCadastrarDenuncias.java
+++ src/main/java/view/TelaCadastrarDenuncias.java
@@ -4,6 +4,7 @@
  */
 package view;
 
+import controller.DenunciaService;
 import model.Denuncia;
 import java.text.ParseException;
 import java.util.logging.Level;
@@ -209,7 +210,7 @@ public class TelaCadastrarDenuncias extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        ////<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
@@ -230,13 +231,6 @@ public class TelaCadastrarDenuncias extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TelaCadastrarDenuncias.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             Logger.getLogger(TelaCadastrarDenuncias.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(TelaCadastrarDenuncias.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(TelaCadastrarDenuncias.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(TelaCadastrarDenuncias.class.getName()).log(Level.SEVERE, null, ex);
-
         }
         //</editor-fold>
         //</editor-fold>
